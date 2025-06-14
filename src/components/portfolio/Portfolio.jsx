dfd78f9b19e091790e1e0ecf1c72e668724a4699
LexberCabanlong <lexber.cabanlong@gmail.com>
2025-06-15 03:58:25 +0800

    Added New Project to Portfolio
    Added Fuel Economy Dashboard

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import { Grid, Card, CardMedia, CardContent, CardActions, Typography, Button } from '@mui/material';
 import { FaGithub } from 'react-icons/fa';
-import DATA_IMG from '../../assets/img/portfolio/data_analytics/data_img.jpg';
+import FUEL_ECON_IMG from '../../assets/img/portfolio/data_analytics/fuel_economy.png';
 import WEB_DEV_IMG_NIA from '../../assets/img/portfolio/web_development/web_dev_nia_document_tracker.png';
 import WEB_DEV_IMG from '../../assets/img/portfolio/web_development/web_dev.png';
 import './portfolio.css';
 
 const projects = [
     { 
+        title: 'Fuel Economy', 
+        type: 'Tableau Dashboard', 
+        year: 2025,
+        description: 
+            'A dashboard designed to analyze and visualize fuel economy data from the U.S. EPA, offering insights into the efficiency ' + 
+            ' of vehicles over time. This currently focuses exclusively on conventional internal combustion engine (ICE) vehicles,' + 
+            ' excluding hybrids and electric models.', 
+        tech:'Tableau',
+        githubLink: '#', 
+        liveLink: 'https://public.tableau.com/app/profile/lexber.cabanlong/viz/FuelEconomy_17499296490290/Overview', 
+        image: FUEL_ECON_IMG 
+    },
+    { 
         title: 'Portfolio Website', 
         type: 'Web Development', 
+        year: 2025,
         description: 
-            'If you are here, then you are already viewing my portfolio. '+
+            'If you are here, then you are already viewing my portfolio. ' +
             'I wanted to learn how to develop websites using React, so I did this portfolio using React. ' +
             'It is a responsive and user-friendly website that should work on any device.' , 
         tech:'React.js, Material UI, Tailwind',
@@ -22,6 +36,7 @@ const projects = [
     { 
         title: 'Document Tracking Information System', 
         type: 'Web Development', 
+        year: 2024,
         description: 
             'It is a document tracking information system designed to ' +
             'manage and monitor the movement of documents within an office. ' +
@@ -34,6 +49,7 @@ const projects = [
         image: WEB_DEV_IMG_NIA 
     },
 ];
+
 
 
 const ProjectShowcase = () => {
@@ -61,6 +77,13 @@ const ProjectShowcase = () => {
                                 sx={{ fontSize: '0.85rem', color: 'text.secondary' }}
                             >
                                 {project.type}
+                            </Typography>
+                            <Typography 
+                                variant="body2" 
+                                color="text.secondary" 
+                                sx={{ fontSize: '0.8rem', color: 'text.secondary' }}
+                            >
+                                {project.year}
                             </Typography>
                             <Typography 
                                 gutterBottom
